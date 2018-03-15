03d1ed80949aedc748f10dd6233d7d3b42191002
NB Jo <nijo@ekspresbank.dk>
2018-03-15 23:09:19 +0100

    Add observable to hero.service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
